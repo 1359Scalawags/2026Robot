7efaac2d54a50606313df8cb215f6c856b04fc7a
NASA-12-1962 <knoxbeaufort@protonmail.com>
2026-01-28 18:09:39 -0800

    LL aim and ranging kp

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -36,6 +36,7 @@ import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -88,7 +89,7 @@ public class SwerveSubsystem extends SubsystemBase
 
   // SparkMax left, right;
   // RelativeEncoder leftEncoder, rightEncoder;
-  AHRS navx;
+  // AHRS navx;
 
   // double                         driveGearRatio      = 1.0;
   // double                         wheelDiameterMeters = 4.0;
@@ -105,11 +106,11 @@ public class SwerveSubsystem extends SubsystemBase
    public SwerveSubsystem(File directory) { 
 
     // differentialDriveKinematics = new DifferentialDriveKinematics(trackWidth);
-    differentialDrivePoseEstimator = new DifferentialDrivePoseEstimator(differentialDriveKinematics,
-                                                                        navx.getRotation2d(),
-                                                                        0,
-                                                                        0,
-                                                                        Pose2d.kZero); // Starting at (0,0)
+    // differentialDrivePoseEstimator = new DifferentialDrivePoseEstimator(differentialDriveKinematics,
+    //                                                                     navx.getRotation2d(),
+    //                                                                     0,
+    //                                                                     0,
+    //                                                                     Pose2d.kZero); // Starting at (0,0)
 
     limelight = new Limelight("limelight");
     limelight.getSettings()
@@ -172,12 +173,12 @@ public class SwerveSubsystem extends SubsystemBase
       // differentialDrivePoseEstimator.update(navx.getRotation2d(), SwerveModule);
 
     // Required for megatag2
-    limelight.getSettings()
-             .withRobotOrientation(new Orientation3d(navx.getRotation3d(),
-                                                     new AngularVelocity3d(DegreesPerSecond.of(0),
-                                                                           DegreesPerSecond.of(0),
-                                                                           DegreesPerSecond.of(0))))
-             .save();
+    // limelight.getSettings()
+    //          .withRobotOrientation(new Orientation3d(navx.getRotation3d(),
+    //                                                  new AngularVelocity3d(DegreesPerSecond.of(0),
+    //                                                                        DegreesPerSecond.of(0),
+    //                                                                        DegreesPerSecond.of(0))))
+    //          .save();
 
     // Get the vision estimate.
     Optional<PoseEstimate> visionEstimate = poseEstimator.getPoseEstimate(); // BotPose.BLUE_MEGATAG2.get(limelight);
@@ -758,4 +759,43 @@ public class SwerveSubsystem extends SubsystemBase
   {
     return swerveDrive;
   }
+
+  // simple proportional turning control with Limelight.
+  // "proportional control" is a control algorithm in which the output is proportional to the error.
+  // in this case, we are going to return an angular velocity that is proportional to the 
+  // "tx" value from the Limelight.
+  double limelight_aim_proportional()
+  {    
+    // kP (constant of proportionality)
+    // this is a hand-tuned number that determines the aggressiveness of our proportional control loop
+    // if it is too high, the robot will oscillate around.
+    // if it is too low, the robot will never reach its target
+    // if the robot never turns in the correct direction, kP should be inverted.
+    double kP = .035;
+
+    // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the rightmost edge of 
+    // your limelight 3 feed, tx should return roughly 31 degrees.
+    double targetingAngularVelocity =  NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0.0) * kP;
+
+    // convert to radians per second for our drive method
+    // targetingAngularVelocity *= Drivetrain.kMaxAngularSpeed;
+
+    //invert since tx is positive when the target is to the right of the crosshair
+    targetingAngularVelocity *= -1.0;
+
+    return targetingAngularVelocity;
+  }
+
+  // simple proportional ranging control with Limelight's "ty" value
+  // this works best if your Limelight's mount height and target mount height are different.
+  // if your limelight and target are mounted at the same or similar heights, use "ta" (area) for target ranging rather than "ty"
+  double limelight_range_proportional()
+  {    
+    double kP = .1;
+    double targetingForwardSpeed = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0.0) * kP;
+    targetingForwardSpeed *= Constants.swerveDrive.MAX_SPEED;
+    targetingForwardSpeed *= -1.0;
+    return targetingForwardSpeed;
+  }
+
 }
