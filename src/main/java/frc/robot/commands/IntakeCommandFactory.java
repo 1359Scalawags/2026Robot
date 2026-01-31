d33f68e653434fa8717c187920f1f4b4d9cfb56c
itsaleclolz <itsaleclolz@gmail.com>
2026-01-30 20:16:04 -0800

    add intake command factory

---
+++ src/main/java/frc/robot/commands/IntakeCommandFactory.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase; 
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class IntakeCommandFactory extends SubsystemBase {
+
+    private final IntakeSubsystem IntakeSubsystem;
+
+    
+    public IntakeCommandFactory(IntakeSubsystem intakeSubsystem) {
+        this.IntakeSubsystem = intakeSubsystem;
+    }
+    
+    // Start the intake command 
+    public Command StartIntake() { 
+        return runOnce(() -> IntakeSubsystem.intakeOn()); 
+    }
+
+    // Stop the intake command 
+    public Command StopIntake() { 
+        return runOnce(() -> IntakeSubsystem.intakeOff()); 
+
+
+
+}
