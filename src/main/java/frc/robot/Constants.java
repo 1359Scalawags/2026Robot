c0c8423b912c1f2ba2b39b8f89412961c8c2934c
PenguinPerson11 <g.mhooncoatney@gmail.com>
2026-02-13 19:44:09 -0800

    Updated intake to YAMS

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -93,8 +93,8 @@ public final class Constants {
   public static class Intake {
     // ========== CONFIGURATION ==========
     // CAN IDs for the motor controllers
-    public static final int KICKER_MOTOR_CAN_ID = 10;
-    public static final int STAR_MOTOR_CAN_ID = 11;
+    public static final int sushiMotorID = 10;
+    public static final int starMotorID = 11;
 
     // Motor speed in RPM
     public static final double INTAKE_SPEED_RPM = 1600;
