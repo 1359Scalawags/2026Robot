0fcb20ecd80d9ebc811786d635c0ffe77ed12cde
NASA-12-1962 <knoxbeaufort@protonmail.com>
2026-02-15 15:43:53 -0800

    working YAGSL code (drives correctly, needs tunning)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -56,8 +56,8 @@ public class RobotContainer {
 
         private final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem(
                         new File(Filesystem.getDeployDirectory(), Constants.swerveDrive.flipper2026));
-        private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
-        private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
+        // private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
+        // private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
         // private final ClimberSubsystem m_ClimberSubsystem = new ClimberSubsystem();
         // private final HopperSubsystem m_HopperSubsystem = new HopperSubsystem();
 
@@ -192,21 +192,21 @@ public class RobotContainer {
                         //=========== Set Default Command  for swerve ============
                     if (RobotBase.isSimulation()) {
                         m_SwerveSubsystem.setDefaultCommand(driveFieldOrientedDirectAngleKeyboard);
-                        m_ShooterSubsystem.setDefaultCommand(m_ShooterSubsystem.stopShooter());
-                        m_IntakeSubsystem.setDefaultCommand(m_IntakeSubsystem.stopIntake());
+                        // m_ShooterSubsystem.setDefaultCommand(m_ShooterSubsystem.stopShooter());
+                        // m_IntakeSubsystem.setDefaultCommand(m_IntakeSubsystem.stopIntake());
                 } else {
                         m_SwerveSubsystem.setDefaultCommand(driveFieldOrientedAnglularVelocity);
                 }
 
 
-                m_AssistantJoystick.button(2).whileTrue(m_ShooterSubsystem.shootFuel(RPM.of(200),RPM.of(200)));
-                m_AssistantJoystick.button(3).whileTrue(m_ShooterSubsystem.setShooterVelocity(RPM.of(500)));
-                m_AssistantJoystick.button(4).whileTrue(m_ShooterSubsystem.setShooterDutyCycle(0.3));
+                // m_AssistantJoystick.button(2).whileTrue(m_ShooterSubsystem.shootFuel(RPM.of(200),RPM.of(200)));
+                // m_AssistantJoystick.button(3).whileTrue(m_ShooterSubsystem.setShooterVelocity(RPM.of(500)));
+                // m_AssistantJoystick.button(4).whileTrue(m_ShooterSubsystem.setShooterDutyCycle(0.3));
 
 
-                m_AssistantJoystick.button(5).whileTrue(m_IntakeSubsystem.setIntakeSpeed(RPM.of(Constants.Intake.sushiIntakeSpeed),RPM.of(Constants.Intake.starIntakeSpeed)));
-                m_AssistantJoystick.button(6).whileTrue(m_IntakeSubsystem.setStarVelocity(RPM.of(Constants.Intake.starIntakeSpeed)));
-                m_AssistantJoystick.button(7).whileTrue(m_IntakeSubsystem.setSushiVelocity(RPM.of(Constants.Intake.sushiIntakeSpeed)));
+                // m_AssistantJoystick.button(5).whileTrue(m_IntakeSubsystem.setIntakeSpeed(RPM.of(Constants.Intake.sushiIntakeSpeed),RPM.of(Constants.Intake.starIntakeSpeed)));
+                // m_AssistantJoystick.button(6).whileTrue(m_IntakeSubsystem.setStarVelocity(RPM.of(Constants.Intake.starIntakeSpeed)));
+                // m_AssistantJoystick.button(7).whileTrue(m_IntakeSubsystem.setSushiVelocity(RPM.of(Constants.Intake.sushiIntakeSpeed)));
                 
 
 //----------------------
@@ -215,8 +215,8 @@ public class RobotContainer {
                 // m_SwerveSubsystem::getX, m_SwerveSubsystem::getY);
 
                 if (RobotBase.isReal()) {
-                        m_DriverJoystick.trigger().onTrue(Commands.runOnce(
-                                () -> m_SwerveSubsystem.zeroGyro()));
+                        // m_DriverJoystick.trigger().onTrue(Commands.runOnce(
+                        //         () -> m_SwerveSubsystem.zeroGyro()));
 
                         Pose2d target = new Pose2d(new Translation2d(1, 4),
                                         Rotation2d.fromDegrees(90));
@@ -233,13 +233,13 @@ public class RobotContainer {
                                                         new Constraints(Units.degreesToRadians(360),
                                                                         Units.degreesToRadians(180))));
 
-                        m_DriverJoystick.trigger().onTrue(Commands.runOnce(
+                        m_DriverJoystick.button(5).onTrue(Commands.runOnce(
                                         () -> m_SwerveSubsystem.resetOdometry(new Pose2d(3, 3, new Rotation2d()))));
 
                         m_DriverJoystick.button(11).onTrue(Commands.runOnce(
                                 () -> m_SwerveSubsystem.zeroGyro()));
 
-                        m_DriverJoystick.button(1).whileTrue(m_SwerveSubsystem.sysIdDriveMotorCommand());
+                        // m_DriverJoystick.button(1).whileTrue(m_SwerveSubsystem.sysIdDriveMotorCommand());
 
 
                         m_DriverJoystick.button(2)
