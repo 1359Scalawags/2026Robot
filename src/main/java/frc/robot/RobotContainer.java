16583922054ec34f01bc1ea6e042bedf6a31a4a1
Dean A. Akin <dean.anthony.akin@gmail.com>
2026-02-15 17:18:28 -0800

    Revert "updated vendor libs"
    This reverts commit 325a14f4b520c783b6be03c8c78538826150395f.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -57,12 +57,11 @@ public class RobotContainer {
         private final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem(
                         new File(Filesystem.getDeployDirectory(), Constants.swerveDrive.flipper2026));
         private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
-        private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
-        // private final ClimberSubsystem m_ClimberSubsystem = new ClimberSubsystem();
-        // private final HopperSubsystem m_HopperSubsystem = new HopperSubsystem();
+        private final IntakeCommandFactory m_IntakeCommandFactory = new IntakeCommandFactory(m_IntakeSubsystem);
+        // TODO: Gavan or Alec; Add Shooter Subystem
+        // TODO:  Gavn or c; Add Climber Subsystem      
 
-        // private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
-        // private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
+        private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
         
         private final CommandJoystick m_DriverJoystick = new CommandJoystick(
                         Constants.OperatorConstants.DriverJoystick);
@@ -195,9 +194,7 @@ public class RobotContainer {
                         //=========== Set Default Command  for swerve ============
                     if (RobotBase.isSimulation()) {
                         m_SwerveSubsystem.setDefaultCommand(driveFieldOrientedDirectAngleKeyboard);
-                        m_ShooterSubsystem.setDefaultCommand(m_ShooterSubsystem.stopShooter());
-                        m_IntakeSubsystem.setDefaultCommand(m_IntakeSubsystem.stopIntake());
-                        // m_ShooterSubsystem.setDefaultCommand(m_ShooterSubsystem.set(0));
+                        m_ShooterSubsystem.setDefaultCommand(m_ShooterSubsystem.set(0));
 
                 } else {
                         m_SwerveSubsystem.setDefaultCommand(driveFieldOrientedAnglularVelocity);
@@ -205,19 +202,17 @@ public class RobotContainer {
 
 
                 m_AssistantJoystick.button(2).whileTrue(m_ShooterSubsystem.shootFuel(RPM.of(200),RPM.of(200)));
-                m_AssistantJoystick.button(3).whileTrue(m_ShooterSubsystem.setShooterVelocity(RPM.of(500)));
-                m_AssistantJoystick.button(4).whileTrue(m_ShooterSubsystem.setShooterDutyCycle(0.3));
-                // m_AssistantJoystick.button(2).whileTrue(m_ShooterSubsystem.shootFuel(RPM.of(200),RPM.of(200)));
 
                 // m_AssistantJoystick.button(2).whileTrue(m_ShooterSubsystem.setShooterVelocity(RPM.of(100)));
 
-                // m_AssistantJoystick.button(3).whileTrue(m_ShooterSubsystem.set(0.3));
+                m_AssistantJoystick.button(3).whileTrue(m_ShooterSubsystem.set(0.3));
 
 
-                m_AssistantJoystick.button(5).whileTrue(m_IntakeSubsystem.setIntakeSpeed(RPM.of(Constants.Intake.sushiIntakeSpeed),RPM.of(Constants.Intake.starIntakeSpeed)));
-                m_AssistantJoystick.button(6).whileTrue(m_IntakeSubsystem.setStarVelocity(RPM.of(Constants.Intake.starIntakeSpeed)));
-                m_AssistantJoystick.button(7).whileTrue(m_IntakeSubsystem.setSushiVelocity(RPM.of(Constants.Intake.sushiIntakeSpeed)));
-                
+
+                //  m_AssistantJoystick.button(6).onTrue(m_IntakeSubsystem.setIntakeSpeed(RPM.of(Constants.Intake.sushiIntakeSpeed),RPM.of(Constants.Intake.starIntakeSpeed)));
+
+                //  m_AssistantJoystick.button(6).onTrue(m_IntakeSubsystem.setIntakeSpeed(RPM.of(Constants.Intake.sushiIntakeSpeed),RPM.of(Constants.Intake.starIntakeSpeed)));
+  
 
 //----------------------
 
