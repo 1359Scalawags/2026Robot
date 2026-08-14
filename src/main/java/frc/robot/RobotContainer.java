e75c575f70d29071c7c57fa8323d174c4b27eeca
itsaleclolz <itsaleclolz@gmail.com>
2026-08-14 11:41:13 -0700

    button 2

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -279,7 +279,7 @@ public class RobotContainer {
                         m_AssistantJoystick.button(16).whileTrue(ClimbDown);
 
                         m_AssistantJoystick.trigger().whileTrue(shootFuel);
-                        m_AssistantJoystick.button(14).whileTrue(Commands.parallel(intakeFuel, upperUp));
+                        m_AssistantJoystick.button(2).whileTrue(Commands.parallel(intakeFuel, upperUp));
                         m_AssistantJoystick.button(4).whileTrue(Commands.parallel(outtakeFuel, upperDown));
                         //m_AssistantJoystick.button(2).whileTrue(flipUp)
 
